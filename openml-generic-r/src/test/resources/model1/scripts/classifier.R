53c04f0758a228f9e80a022ce3826e7299b0ac01
Shenguito <swggh@iscte-iul.pt>
2019-10-07 10:51:37 +0100

    openml-generic-r: Update the models for classification
    In the previous commit, model0 and model1 were swapped. However there are tests that depended on the model0 did not pass.
    
    Swap the models back, and change the return value for each models to the other one in comparison with two previous commit to solve this problem.

--- openml-generic-r/src/test/resources/model1/scripts/classifier.R
+++ openml-generic-r/src/test/resources/model1/scripts/classifier.R
@@ -3,9 +3,7 @@ loadModel <- function() {
 }
 
 getClassDistribution <- function(instance) {
-    set.seed(instance[1,1])
-    aDist = runif(1)
-    return(data.frame(a = aDist, b = 1 - aDist))
+    return(data.frame(a=0.6,b=0.4))
 }
 
 classify <- function(instance) {
